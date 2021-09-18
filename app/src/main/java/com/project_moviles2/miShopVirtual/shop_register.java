3cf67c042ef7d8fe0425f1e2f9db6784ec39bdef
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-09-18 05:55:28 -0500

    se crean los layout registr e inicio sesion

---
+++ app/src/main/java/com/project_moviles2/miShopVirtual/shop_register.java
@@ -0,0 +1,14 @@
+package com.project_moviles2.miShopVirtual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shop_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_register);
+    }
+}
