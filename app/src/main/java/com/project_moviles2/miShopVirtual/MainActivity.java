818067c7ca9e44eb45a3c2b6ed4ca43b3fb4cca1
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-10-01 12:39:55 -0500

    se modifican registro y main iniciar sesion

--- app/src/main/java/com/project_moviles2/miShopVirtual/MainActivity.java
+++ app/src/main/java/com/project_moviles2/miShopVirtual/MainActivity.java
@@ -2,9 +2,20 @@ package com.project_moviles2.miShopVirtual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView jtvmensaje1;
+    EditText jetemail, jetpassword;
+    Button jbtiniciar, jbtregistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        jtvmensaje1=findViewById(R.id.tvmensaje1);
+        jetemail=findViewById(R.id.etemail);
+        jetpassword=findViewById(R.id.etpassword);
+        jbtiniciar=findViewById(R.id.btiniciar);
+        jbtregistrar=findViewById(R.id.btregistrar);
+
+        Animation animation1= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.desplazamiento_abajo);
+
+        jtvmensaje1.setAnimation(animation1);
+
+    }
+    public void Register (View view){
+
+        Intent intent= new Intent(getApplicationContext(),shop_register.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/project_moviles2/miShopVirtual/shop_register.java
+++ app/src/main/java/com/project_moviles2/miShopVirtual/shop_register.java
@@ -2,13 +2,38 @@ package com.project_moviles2.miShopVirtual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class shop_register extends AppCompatActivity {
+
+    TextView jtvmensaje2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_register);
+
+        getSupportActionBar().hide();
+
+        jtvmensaje2=findViewById(R.id.tvmensaje2);
+
+
+
+
+        Animation animation1= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.desplazamiento_abajo);
+
+        jtvmensaje2.setAnimation(animation1);
+
+    }
+    public void Back (View view){
+
+        Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+
     }
 }
